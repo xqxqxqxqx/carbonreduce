ce788ae4a9c894a88aa6f9a4f376f5875ff057b6
Jiaqi (Jessie) Zheng <zhengjiaqi.jessie@gmail.com>
2022-09-23 12:08:23 +0200

    Refine slider styling

--- packages/ing-slider-custom/src/IngSliderCustom.js
+++ packages/ing-slider-custom/src/IngSliderCustom.js
@@ -1,6 +1,7 @@
 import {
   css,
   IngInputRange,
+  orange,
 } from 'ing-web';
 
 export class IngSliderCustom extends IngInputRange {
@@ -10,10 +11,15 @@ export class IngSliderCustom extends IngInputRange {
       css`
         :host {
           color: white;
+          text-align: start;
         }
 
         :host .input-group__container {
           background-color: black;
+        }
+
+        :host .form-field__group-two > div:first-child {
+          color: ${orange};
         }
 
         .form-field__label ::slotted(*) {
