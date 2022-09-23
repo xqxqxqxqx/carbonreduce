444ceae49433ea3b12bdd98435c0b5b7597424a7
Jean-Pierre Bonnet <bonnetjeanpierre0@gmail.com>
2022-09-23 08:43:44 +0200

    Updated formulas functions

--- helpers/formulas.js
+++ helpers/formulas.js
@@ -1,13 +1,13 @@
 // TODO: Update functions and implement formulas
 
-export function calcHomeUsage(floorSize, insulation, insideTemp, renewableEnergy) {
+export function calcHomeUsage(gasUsage, electricityUsage) {
   return Array.from({length: 100}, () => Math.floor(Math.random() * 100));
-  // return  floorSize / insulation + insideTemp - renewableEnergy;
+  // return  gasUsage / electricityUsage + insideTemp - renewableEnergy;
 }
 
-export function calcOfficeUsage(floorSize, insulation, insideTemp, renewableEnergy) {
+export function calcOfficeUsage(gasUsage, electricityUsage) {
   return Array.from({length: 100}, () => Math.floor(Math.random() * 100));
-  // return  floorSize / insulation + insideTemp - renewableEnergy;
+  // return  gasUsage / electricityUsage + insideTemp - renewableEnergy;
 }
 
 export function calcTravelUsage(kmPerWeek, mode) {
@@ -15,34 +15,28 @@ export function calcTravelUsage(kmPerWeek, mode) {
   // return  kmPerWeek + mode;
 }
 
-export function calcHomeHeatingUsage(a, b) {
-  return 839;
-  // return  a + b;
+export function calcHomeGasUsage(inputs) {
+  return  2*inputs["gasUsage"] + inputs["electricityUsage"];
 }
 
-export function calcHomeElectricityUsage(a, b) {
-  return 235;
-  // return  a + b;
+export function calcHomeElectricityUsage(inputs) {
+return  inputs["gasUsage"] + inputs["electricityUsage"];
 }
 
-export function calcOfficeHeatingUsage(a, b) {
-  return 642;
-  // return  a + b;
+export function calcOfficeGasUsage(inputs) {
+  return inputs["gasUsage"]/70 + 2*inputs["electricityUsage"];
 }
 
-export function calcOfficeElectricityUsage(a, b) {
-  return 432;
-  // return  a + b;
+export function calcOfficeElectricityUsage(inputs) {
+  return inputs["gasUsage"]/50 + inputs["electricityUsage"];
 }
 
-export function calcCarUsage(a, b) {
-  return 123;
-  // return  a + b;
+export function calcCarUsage(inputs) {
+  return 2*inputs["kilometersPerWeek"];
 }
 
-export function calcPublicTransportUsage(a, b) {
-  return 12;
-  // return  a + b;
+export function calcNonCarUsage(inputs) {
+  return inputs["kilometersPerWeek"];
 }
 
 export function calcTotalUsage(homeConsumption, officeConsumption, travelConsumption) {
